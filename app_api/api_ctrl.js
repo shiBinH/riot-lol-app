ddbc8d655fa5b6c8771b0d78ab24471aaa950ea0
Shi B <sh2581@nyu.edu>
2018-10-10 05:33:13 +0000

    Basic route handling for /wheres_the_jungler

--- app_api/api_ctrl.js
+++ app_api/api_ctrl.js
@@ -1,9 +1,53 @@
-const router = require('express').Router();
+const express = require('express')
+const router = express.Router();
+const path  = require('path');
+
+
+const get_data_by_date_handler = (req, res, next) => {
+  console.log('GET handler of:', req.originalUrl, 'params:', req.params)
+  res.json({
+    date: req.params.date
+    //  date: (new Date()).toDateString().replace(/ /g, '_')
+  });
+}
+
+const get_minimaps_by_date_handler = (req, res, next) => {
+  console.log('GET handler of:', req.originalUrl, 'params:', req.params)
+  res.json({
+    status: 'success!'
+  });
+}
+
+const get_images_by_date_main_handler = express.Router();
+get_images_by_date_main_handler
+  .get('/minimaps/:timestamp', get_minimaps_by_date_handler);
+
+const get_images_by_date_entry_handler = (req, res, next) => {
+  req.test = req.params
+  next();
+}
 
 router
-  .use('/', (req, res, next) => {
-    console.log('Inside handler of: ' + req.url);
-    res.send('FROM API');
-  })
+  .get('/data/by-date/:date', get_data_by_date_handler)
+  .use(
+    '/images/by-date/:date', 
+    get_images_by_date_entry_handler, 
+    get_images_by_date_main_handler
+  );
+    
+  
+  /*
+  
+  date:
+    /wheres_the_jungler/data/{date}
+  
+  images:
+    /wheres_the_jungler/icons/{champion info}
+  
+    /wheres_the_jungler/images/by-date/{date}/minimaps/{timestamp}
+    /wheres_the_jungler/images/by-date/{date}/scoreboards/{timestamp}
+    
+  
+  */
 
 module.exports = router;
