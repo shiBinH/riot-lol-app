c81e3ddfdb324371355b5ea4c84c89099cf5c3bd
Shi B <sh2581@nyu.edu>
2018-10-12 05:39:03 +0000

    Connected get/post data handlers to dynamodb

--- app_api/api_ctrl.js
+++ app_api/api_ctrl.js
@@ -2,19 +2,96 @@ const express = require('express');
 const router = express.Router();
 const bodyParser = require('body-parser');
 const request = require('request');
+const AWS = require('aws-sdk');
+
+AWS.config.update({
+  region: 'us-east-2',
+  endpoint: 'https://dynamodb.us-east-2.amazonaws.com'
+})
+
+const docClient = new AWS.DynamoDB.DocumentClient();
+
+const post_data_by_date_handler = (req, res, next) => {
+  
+  let date = req.params.date
+  const new_item = {
+    TableName: 'Wheres-the-Jungler',
+    Item: {
+      'date': date,
+      'meta_data': {
+        'league': {
+          'min': 'p1',
+          'max': 'd4'
+        },
+        'patch': 8.22
+      },
+      'time_slices': [
+        {
+          'time_stamp': {
+            'min': 5,
+            'sec': 41
+          },
+          'images': {
+            'minimap': 'minimap1_5-41.png',
+            'scoreboard': 'scoreboard_5-51.png'
+          },
+          'champions': [
+            {
+              'id': 12,
+              'location': {
+                x: 123,
+                y: 456
+              }
+            }  
+          ]
+        }
+      ]
+    },
+    ReturnValues: 'ALL_OLD'
+  }
+  
+  docClient.put(
+    new_item,
+    (err, data) => {
+      if (err) {
+        console.error('Unable to add item. Error:', JSON.stringify(err, null, 4));
+      } else {
+        console.log('Successfully added item:', JSON.stringify(data, null, 4))
+        
+      }
+    }
+  )
+  
+  res.end();
+}
 
 const get_data_by_date_handler = (req, res, next) => {
-  console.log('GET handler of:', req.originalUrl)
-  res.json({
-    status: "SUCCESS!"
-  });
+  
+  const query = {
+    TableName: 'Wheres-the-Jungler',
+    Key: {
+      'date': req.params.date//  (new Date()).toDateString()
+    }
+  }
+  
+  docClient.get(
+    query,
+    (err, data) => {
+      if (err) {
+        console.err('Unalbe to get item. Error:', JSON.stringify(err, null, 4));
+      } else {
+        console.log('Successfully got item:', JSON.stringify(data, null, 4));
+        res.json(data);
+      }
+    }
+  )
 }
 
 const get_minimaps_by_date_handler = (req, res, next) => {
-  console.log('GET handler of:', req.originalUrl)
+  console.log('GET handler of:', req.originalUrl);
   res.json({
-    status: 'success!'
-  });
+    status: 'Success!'
+  })
 }
 
 const get_scoreboards_by_date_handler = (req, res, next) => {
@@ -25,8 +102,8 @@ const get_scoreboards_by_date_handler = (req, res, next) => {
 }
 
 const get_images_by_date_main_handler = express.Router()
-  .get('/minimaps/:timestamp', get_minimaps_by_date_handler)
-  .get('/scoreboards/:timestamp', get_scoreboards_by_date_handler);
+  .get('/minimaps', get_minimaps_by_date_handler)
+  .get('/scoreboards', get_scoreboards_by_date_handler);
 
 const get_images_by_date_entry_handler = (req, res, next) => {
   console.log('\nENTRY handler of:', req.originalUrl )
@@ -36,13 +113,12 @@ const get_images_by_date_entry_handler = (req, res, next) => {
 const test_handler = (req, res, next) => {
   if (process.env.TEST) {
     request({
-      url: 'https://riot-lol-app-xyrho.c9users.io/wheres_the_jungler/images/add/by-date/wed_oct_10_2018',
-      method: 'POST',
-      json: true,
-      body: {
-        key1: 'val1',
-        key2: 'val2'
-      }
+      url: 'https://riot-lol-app-xyrho.c9users.io/Wheres-the-Jungler/images/date/' 
+            + req.query.date + '/' 
+            + req.query.type + '/?timestamp='
+            + req.query.timestamp,
+      method: 'GET',
+      json: true
       
     },
       (err, response, body) => {
@@ -60,15 +136,19 @@ router
   .use(bodyParser.json())
   //  get handlers
   .get('/test', test_handler)
-  .get('/data/by-date/:date', get_data_by_date_handler)
+  .get('/data/date/:date', get_data_by_date_handler)
   .use(
-    '/images/by-date/:date', 
+    '/images/date/:date', 
     get_images_by_date_entry_handler, 
     get_images_by_date_main_handler
   )
   //  post handlers
   .post(
-    '/images/add/by-date/:date', 
+    '/data/add/date/:date', 
+    post_data_by_date_handler
+  )
+  .post(
+    '/images/add/date/:date', 
     (req, res, next) => {
       console.log('POST handler of', req.originalUrl);
       console.log('BODY:', req.body);
@@ -80,13 +160,13 @@ router
   /*
   
   date:
-    /wheres_the_jungler/data/by-date/{date}
+    /Wheres-the-Jungler/data/by-date/{date}
   
   images:
-    /wheres_the_jungler/icons/{champion info}
+    /Wheres-the-Jungler/icons/{champion info}
   
-    /wheres_the_jungler/images/by-date/{date}/minimaps/{timestamp}
-    /wheres_the_jungler/images/by-date/{date}/scoreboards/{timestamp}
+    /Wheres-the-Jungler/images/by-date/{date}/minimaps?timestamp=5:41
+    /Wheres-the-Jungler/images/by-date/{date}/scoreboards?timestamp=5:41
     
   
   */
