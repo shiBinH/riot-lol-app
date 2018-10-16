b32e89cd29d1826a708774073cf40216e544fa0d
Shi B <sh2581@nyu.edu>
2018-10-16 23:45:05 +0000

    Added API endpoint to get list of available dates

--- app_api/api_ctrl.js
+++ app_api/api_ctrl.js
@@ -6,6 +6,7 @@ const upload = multer();
 const request = require('request');
 const AWS = require('aws-sdk');
 const s3 = new AWS.S3({apiVersion: '2006-03-01'});
+const fs = require('fs');
 
 
 //  AWS_ACCESS_KEY_ID= AWS_SECRET_ACCESS_KEY TEST=true nodemon app.js
@@ -75,12 +76,11 @@ const post_data_by_date_handler = (req, res, next) => {
   res.end();
 }
 
-
 const get_data_by_date_handler = (req, res, next) => {
   /**
    * 
-   *  Update handler to reflect:
-   *    /Wheres-the-Jungler/data/date/{date}
+   * Endpoint:
+   *  wheres-the-jungler/data/date
    *  
    * 
    */
@@ -114,7 +114,6 @@ const get_data_by_date_handler = (req, res, next) => {
 }
 
 const get_minimaps_by_date_handler = (req, res, next) => {
-  
   function createKey(date, gameId, min, sec, format) {
     //  need to validate input
     return  date + '_' +
@@ -131,7 +130,7 @@ const get_minimaps_by_date_handler = (req, res, next) => {
     Key: createKey(req.params.date, req.params.gameId, req.query.min, req.query.sec, req.query.format)
   }
   
-  //  /wheres-the-jungler/date/{date}/game/{gameId}/minimaps?min=5&sec=41&format=png
+  //  /wheres-the-jungler/images/date/{date}/game/{gameId}/minimaps?min=5&sec=41&format=png
   s3.getObject(
     params,
     (err, data) => {
@@ -160,9 +159,18 @@ const get_scoreboards_by_date_handler = (req, res, next) => {
 }
 
 const get_dates_handler = (req, res, next) => {
+  /**
+   * Endpoint:
+   *  wheres-the-jungler/data/dates
+   * 
+   * Returns JSON data containing dates for which data
+   * is available.
+   * 
+   * TODO:
+   *  Add explanations to code
+   * 
+   * */
   
-  //  endpoint:
-  //    wheres-the-jungler/data/dates
   
   AWS.config.update({
     region: 'us-east-2',
@@ -173,9 +181,11 @@ const get_dates_handler = (req, res, next) => {
   
   const query = {
     TableName: 'Wheres-the-Jungler',
-    Key: {
-      'date': req.params.date //  (new Date()).toDateString()
+    ProjectionExpression: '#d',
+    ExpressionAttributeNames: {
+      '#d': 'date'
     }
+    
   }
   
   docClient.scan(
@@ -184,19 +194,43 @@ const get_dates_handler = (req, res, next) => {
       if (err) {
         console.error('Unable to get item. Error:', JSON.stringify(err, null, 4));
       } else {
-        console.log('Successfully got item:', JSON.stringify(data, null, 4));
-        res.json(data);
+        console.log('Successfully got items');
+        
+        let returnData = {
+          dates: extractDates(data)
+        }
+        
+        res.json(returnData);
       }
     }
   )
+  
+  /*  Helper functions  */
+  /**********************/
+  function extractDates(awsData) {
+    //  Takes DynamoDB returned query and extracts
+    //  date strings into an array
+    
+    let dates = [];
+    let dict = {};
+    let items = awsData.Items
+    for (let i in items) {
+      if (!dict[items[i].date]) {
+        dict[items[i].date] = true;
+        dates.push(items[i].date);
+      }
+    }
+    return dates;
+  }
 }
 
 const test_handler = (req, res, next) => {
   if (process.env.TEST) {
     
     /*
+    
+    //  for testing API calls
     request({
-
       //  url: 'https://riot-lol-app-xyrho.c9users.io/Wheres-the-Jungler/images/date/Wed Oct 11 2018/game/1/minimaps?min=5&sec=41&type=png',
       url: 'https://riot-lol-app-xyrho.c9users.io/wheres-the-jungler/data/add/date/Mon Oct 15 2018',
       method: 'POST',
@@ -209,14 +243,17 @@ const test_handler = (req, res, next) => {
     )
     */
     
-    console.log(req.file)
+    //  for testing image uploading and multer middle ware
+    console.log(req.files[1])
+    /*
     AWS.config.update({region: 'REGION'});
     const params = {
-        Bucket: 'wheres-the-jungler-minimaps',
-        Key: req.file.originalname,
-        Body: req.file.buffer
-      }
+      Bucket: 'wheres-the-jungler-minimaps',
+      Key: req.file.originalname,
+      Body: req.file.buffer
+    }
     
+
     s3.upload(
       params,
       (err, data) => {
@@ -227,7 +264,7 @@ const test_handler = (req, res, next) => {
       }
     )
     
-    
+    */
     res.end();
   }
   
@@ -240,13 +277,14 @@ const test_handler = (req, res, next) => {
 router
   //  entry
   .use(bodyParser.json())
-  //  get handlers
+  //  test handlers
   .get('/test', test_handler)
-  .post('/test', upload.single('sample'), test_handler)
+  .post('/test', upload.array('first'), test_handler)
+  //  get handlers
   .get('/data/dates', get_dates_handler)
   .get('/data/date/:date', get_data_by_date_handler)
-  .get('/images/date/:date/game/:gameId/minimaps', get_minimaps_by_date_handler)
-  .get('/images/date/:date/game/:gameId/scoreboards', get_scoreboards_by_date_handler)
+  .get('/images/date/:date/games/:gameId/minimaps', get_minimaps_by_date_handler)
+  .get('/images/date/:date/games/:gameId/scoreboards', get_scoreboards_by_date_handler)
   //  post handlers
   .post('/data/add/date/:date', post_data_by_date_handler)
   .post(
